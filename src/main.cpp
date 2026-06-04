d57b76a02e127cba2e181592277b86a8623f7a1b
Anatoly Glukhovskoy <Glukhovskoy@impt.ad.uni-h.de>
2026-06-04 22:06:08 +0200

    Initial commit: RP2040-based stepper controller with blink test

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+﻿#include <Arduino.h>
+
+const int LED_PIN = 25; // On-board LED on Raspberry Pi Pico
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(LED_PIN, HIGH);
+  delay(100);
+  digitalWrite(LED_PIN, LOW);
+  delay(300);
+}
